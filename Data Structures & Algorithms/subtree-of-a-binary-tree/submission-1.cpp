5f87fd7d7d8f5af9bebbb25e9128dafb54965c03
revanth kumar urabavi <53334177+revanthitsme@users.noreply.github.com>
2026-08-25 23:11:10 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSametree(TreeNode* r1, TreeNode* r2)
+    {
+        if (!r1 && !r2) return true;
+        if (!r1 && r2) return false;
+        if (r1 && !r2) return false;
+
+        if (r1->val == r2->val &&
+            isSametree(r1->left, r2->left) &&
+            isSametree(r1->right, r2->right))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot) return true;
+        if (!root && subRoot) return false;
+        if (root && !subRoot) return false;
+
+        if (root->val == subRoot->val &&
+            isSametree(root->left, subRoot->left) &&
+            isSametree(root->right, subRoot->right))
+        {
+            return true;
+        }
+        else
+        {
+            if (isSubtree(root->left, subRoot))
+                return true;
+
+            if (isSubtree(root->right, subRoot))
+                return true;
+            
+            return false;
+        }
+    }
+};
